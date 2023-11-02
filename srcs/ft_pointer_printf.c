bc630837438c1fce08a1dd6e3ad9e5840c1d4e76
Will <william.keiser67@gmail.com>
2023-11-02 11:04:30 +0100

    test

--- srcs/ft_pointer_printf.c
+++ srcs/ft_pointer_printf.c
@@ -6,7 +6,7 @@
 /*   By: wkeiser <wkeiser@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 09:22:51 by wkeiser           #+#    #+#             */
-/*   Updated: 2023/10/24 16:19:14 by wkeiser          ###   ########.fr       */
+/*   Updated: 2023/11/02 11:02:51 by wkeiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ void	ft_pointer_printf(void *args, int *count)
 	hex_digits = "0123456789abcdef";
 	i = sizeof(buffer) - 1;
 	buffer[i--] = '\0';
-	if (value == 0)
+/*	if (value == 0)
 	{
 		write(1, "0x0", 3);
 		(*count) += 3;
-	}
+	}*/
 	else
 	{
 		while (value != 0)
@@ -35,6 +35,8 @@ void	ft_pointer_printf(void *args, int *count)
 			buffer[i--] = hex_digits[value % 16];
 			value /= 16;
 		}
+		if (value == 0)
+			buffer[i--] = '0';
 		buffer[i--] = 'x';
 		buffer[i--] = '0';
 		ft_string_printf(&buffer[i + 1], count);
